6b05313086ab79afe76dc4f1b7f08f37f4957718
Billie Muzzy <billiemj@gmail.com>
2019-05-15 21:01:20 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import packages
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import CSS
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//import components
+import App from "./App";
+import AppNavbar from "./components/layouts/AppNavbar";
+
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(<App />, document.getElementById("root"));
